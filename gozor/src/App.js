e4c534a3fe2f3753a6b5d1d2da3e624638eca372
basmallasaid <basmala30311@gmail.com>
2024-11-25 04:32:48 +0200

    update footer

--- gozor/src/App.js
+++ gozor/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import Footer from './Components/Footer';
-import Contact from './Components/Contact'
-import Header from './Components/Header';
+// import Contact from './Components/Contact'
+// import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      {/* <Contact/> */}
       <Footer/>
-      <Header/>
+      {/* <Header/> */}
       
     </div>
   );

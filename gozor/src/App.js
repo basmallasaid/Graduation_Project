17900153b41ffb0f7785da1e502c79f2a3fda234
basmallasaid <basmala30311@gmail.com>
2024-11-25 21:33:42 +0200

    create nav

--- gozor/src/App.js
+++ gozor/src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import StanderErrorBoundary from './Components/Error/StanderErorrBoundary';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Notfound from './Components/Notfound';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer';
-// import Contact from './Components/Contact'
-// import Header from './Components/Header';
+import Navbar from './Components/Navbar';
 function App() {
   return (
       <StanderErrorBoundary>
         <BrowserRouter>
           <Routes>
-          <Route path="/" element={<Footer/>} />
-          <Route path="*" element={<Notfound />} />
+          {/* <Route path="/Home" element={<Footer/>} /> */}
+          <Route path="/" element={<><Navbar/> <Footer/></>} />
+          {/* <Route path="*" element={<Notfound />} /> */}
           </Routes>
         </BrowserRouter>
       </StanderErrorBoundary>
